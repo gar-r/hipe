6a972fd2c6ee50651861514d7603561c54be22ee
Richard Garai <garric@okki.hu>
2025-07-17 21:49:57 +0200

    make login aura blocking more aggressive

--- main.lua
+++ main.lua
@@ -12,41 +12,46 @@ frame:RegisterEvent("PLAYER_REGEN_ENABLED")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:RegisterEvent("ADDON_LOADED")
 frame:SetScript("OnEvent", function(self, event, ...)
-	self[event](self, ...)
+    self[event](self, ...)
 end)
 
 function frame:ADDON_LOADED(addonName)
-	if name == addonName then
-		HipeConf = HipeConf or settings.defaults
-		settings:Init()
-	end
+    if name == addonName then
+        HipeConf = HipeConf or settings.defaults
+        settings:Init()
+    end
 end
 
 function frame:PLAYER_ENTERING_WORLD()
-    blocker:removeAllStandard()
+    -- aggressively queue a couple of aura removals after login/zone-change
+    for i = 1, 5, 1 do
+        C_Timer.After(i, function()
+            blocker:removeAllStandard()
+        end)
+    end
 end
 
 function frame:UNIT_AURA(_, updateInfo)
-	if HipeConf.instantHide then
-		if updateInfo and updateInfo.addedAuras then
-			for _, aura in pairs(updateInfo.addedAuras) do
-				blocker:removeIfBlocked(aura.spellId)
-			end
-		end
-	end
+    if HipeConf.instantHide then
+        if updateInfo and updateInfo.addedAuras then
+            for _, aura in pairs(updateInfo.addedAuras) do
+                blocker:removeIfBlocked(aura.spellId)
+            end
+        end
+    end
 end
 
 function frame:UNIT_SPELLCAST_STOP()
-	blocker:removeAllStandard()
+    blocker:removeAllStandard()
 end
 
 function frame:UNIT_SPELLCAST_CHANNEL_STOP()
-	blocker:removeAllStandard()
-	if not HipeConf.ignoreFishing then
-		blocker:removeFishing()
-	end
+    blocker:removeAllStandard()
+    if not HipeConf.ignoreFishing then
+        blocker:removeFishing()
+    end
 end
 
 function frame:PLAYER_REGEN_ENABLED()
-	blocker:removeAllStandard()
+    blocker:removeAllStandard()
 end
